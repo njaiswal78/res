9d4856d608d0f6bdccbe77971d601e3e822cc351
Natrajan Jaiswal <njaiswal78@gmail.com>
2017-12-14 22:52:32 +0530

    Responsive blocks

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DigiSpread</title>
+	<link rel="stylesheet" type="text/css" href="content.css">
+	</head>
+<body>
+<h1>Initiating Project</h1>
+
+<div id="box">
+	<p><img src="cat.jpeg"/></p>
+	<p><img src="dog.jpeg"/></p>
+	<p><img src="bird.jpeg"/></p>
+</div>
+</body>
+</html>
